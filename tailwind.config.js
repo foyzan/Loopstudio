b5d2f6c00d45f84ba923b4c2184dcafc9c07646a
foyzan <foyzanahmedapon01@gmail.com>
2024-09-02 19:47:37 +0600

    hero section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1140px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
